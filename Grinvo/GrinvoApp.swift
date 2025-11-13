6e884cdc50fa709bb637ef46f6235f97f5cb2514
Diego Lopes <diegolopes0501@gmail.com>
2025-11-13 10:46:15 -0300

    Initial Commit

---
+++ Grinvo/GrinvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrinvoApp.swift
+//  Grinvo
+//
+//  Created by Diego Lopes on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GrinvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
